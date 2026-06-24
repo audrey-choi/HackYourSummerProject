d55715b87a4a6816c394edf58ada0b155de086cd
Peter Lin <plin13@calpoly.edu>
2026-06-24 15:42:04 -0700

    test edit 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Build-A-Prosthetic")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."+"\n"+"hello world"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."+"\n"+"hello world!"
 )
