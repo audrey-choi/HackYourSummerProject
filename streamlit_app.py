d27ea322fbfbfe0bb056f2cba8bb1cf7571b6e88
Peter Lin <plin13@calpoly.edu>
2026-06-24 15:48:45 -0700

    line 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🎈 Build-A-Prosthetic")
+st.title("Build-A-Prosthetic")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-    "hello world!"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."+"\n"+"hello world!"
 )
+st.write("What would you like to look for? We have many models, parts, and miscellaneous additions for your dream prosthetic!")
